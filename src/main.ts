71c7676daaacc588b7e04ab4ebe7d81c7305cbf8
chargeling <gtmddly@gmail.com>
2024-10-22 17:02:31 -0700

    Step 1 Complete

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,17 @@
 import "./style.css";
 
-const APP_NAME = "Hello";
+const APP_NAME = "Brainstorming Board";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
 app.innerHTML = APP_NAME;
+
+const title = document.createElement("h1");
+title.textContent = APP_NAME;
+app.appendChild(title);
+
+const canvas = document.createElement("canvas");
+canvas.width = 256;
+canvas.height = 256;
+canvas.id = "drawingCanvas";
+app.appendChild(canvas);
